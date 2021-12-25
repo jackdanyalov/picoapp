4f9f617351cff57534225f1c7cf77cda35112501
Jeyhun Danyalov <jeyhun_danyalov@epam.com>
2021-12-25 19:23:40 +0100

    Initial Commit

---
+++ PicoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicoApp
+//
+//  Created by Jeyhun Danyalov on 25.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
